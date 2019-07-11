8dd9c6b57da0cd001fd3f978c9631ac712936ff6
andyboythekid <andyboythekid@gmail.com>
2019-07-11 10:30:38 -0600

    update examples

---
+++ example/src/entry-example.tsx
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {
+  Navigator,
+  Tabs,
+  Link,
+  NavigatorScreen,
+  Headers,
+  Header,
+} from 'react-navigation-library';
+import { TextInput, BorderlessButton } from 'react-native-gesture-handler';
+
+function Entry({  }: NavigatorScreen) {
+  return (
+    <Navigator routes={['signup', '/', 'login']}>
+      {({ navigate, state }: any) => (
+        <>
+          <Headers>
+            <Header>
+              <Text style={styles.title}>Signup</Text>
+            </Header>
+            <Header>
+              <Text style={styles.title}>Select Option</Text>
+            </Header>
+            <Header>
+              <Text style={styles.title}>Login</Text>
+            </Header>
+          </Headers>
+
+          <Tabs>
+            <Signup navigate={navigate} />
+            <SelectionScreen email={state.email} />
+            <Login navigate={navigate} />
+          </Tabs>
+        </>
+      )}
+    </Navigator>
+  );
+}
+
+function Signup({ navigate }: any) {
+  function handleSubmit(email: string, password: string) {
+    // return to selection screen
+    navigate('./', { email, password });
+  }
+
+  return (
+    <Screen background="coral">
+      <Form title="Signup" onSubmit={handleSubmit} />
+    </Screen>
+  );
+}
+
+function Login({ navigate }: any) {
+  function handleSubmit(email: string, password: string) {
+    // return to selection screen
+    navigate('./', { email, password });
+  }
+
+  return (
+    <Screen background="cadetblue">
+      <Form title="Login" onSubmit={handleSubmit} />
+    </Screen>
+  );
+}
+
+function SelectionScreen({ email = '' }: any) {
+  return (
+    <Screen background="aquamarine">
+      {Boolean(email) && (
+        <Text
+          style={[styles.title, { flex: 0, marginVertical: 20 }]}
+        >{`Thanks ${email}!`}</Text>
+      )}
+
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <Link to="signup" style={styles.button}>
+          <Text>Signup</Text>
+        </Link>
+        <Link to="login" style={styles.button}>
+          <Text>Login</Text>
+        </Link>
+      </View>
+    </Screen>
+  );
+}
+
+function Form({ onSubmit }: any) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {
+    onSubmit(email, password);
+  }
+
+  return (
+    <Screen>
+      <View style={{ flex: 1, marginTop: 20, paddingHorizontal: 30 }}>
+        <TextInput
+          placeholder="Enter email"
+          textContentType="emailAddress"
+          autoCapitalize="none"
+          autoCompleteType="email"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder="Enter password"
+          textContentType="password"
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+          style={styles.input}
+        />
+
+        <BorderlessButton onPress={handleSubmit} style={styles.button}>
+          <Text style={{ textAlign: 'center' }}>Submit</Text>
+        </BorderlessButton>
+      </View>
+    </Screen>
+  );
+}
+
+function Screen({ children, background = 'transparent' }: any) {
+  return (
+    <View style={{ flex: 1, backgroundColor: background }}>{children}</View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+
+  button: {
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    marginVertical: 10,
+  },
+
+  input: {
+    borderWidth: 1,
+    marginVertical: 10,
+    fontSize: 16,
+    padding: 5,
+    height: 40,
+    borderRadius: 4,
+    width: '100%',
+  },
+});
+
+export { Entry };
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,295 +1,100 @@
 // app entry
-import React, { useState, Children } from 'react';
+import React from 'react';
 import {
   AppRegistry,
   Platform,
   View,
   Text,
-  ViewStyle,
-  StyleProp,
-  Button,
   SafeAreaView,
+  StyleSheet,
 } from 'react-native';
-import {
-  Link,
-  Navigator,
-  Tabbar,
-  Tab,
-  Tabs,
-  Stack,
-  Pager,
-  Headers,
-  Header,
-  NavigatorScreen,
-} from 'react-navigation-library';
-
-import { FlatList } from 'react-native-gesture-handler';
-
-function PagerTest() {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  function handleChange(index: number) {
-    setActiveIndex(index);
-  }
-
-  const children = Array.from({ length: activeIndex + 2 });
-
-  return (
-    <View style={{ flex: 1 }}>
-      <Pager
-        index={activeIndex}
-        onChange={handleChange}
-        numberOfScreens={children.length}
-        type="stack"
-      >
-        {children.map((c, i) => {
-          return (
-            <MyScreen key={i} style={{ backgroundColor: 'white' }}>
-              <Text>{`Index: ${i}`}</Text>
-            </MyScreen>
-          );
-        })}
-      </Pager>
-
-      <View style={{ height: 100, width: '100%' }}>
-        <Button title="Inc" onPress={() => setActiveIndex(activeIndex + 1)} />
-        <Button title="Dec" onPress={() => setActiveIndex(activeIndex - 1)} />
-      </View>
-    </View>
-  );
-}
+import { Entry } from './entry-example';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Link, Navigator, Tabs, BOTTOM_SPACE } from 'react-navigation-library';
 
 function App() {
+  const routes = ['/', '/login'];
+
   return (
     <AppContainer>
-      <Navigator routes={['entry', 'app']} initialPath="/entry">
-        <Tabs>
-          <Entry unmountOnExit />
-          <Feeds unmountOnExit />
-        </Tabs>
-      </Navigator>
+      <Navigator routes={routes}>
+        {({ index }: any) => (
+          <>
+            <Tabs pan={{ enabled: false }}>
+              <Index routes={routes} />
+              <Entry unmountOnExit />
+            </Tabs>
 
-      {/* <PagerTest /> */}
+            {index !== 0 && (
+              <Link to="/" style={styles.homeButton}>
+                <Text style={styles.title}>Return Home</Text>
+              </Link>
+            )}
+          </>
+        )}
+      </Navigator>
     </AppContainer>
   );
 }
 
-function Entry({  }: NavigatorScreen) {
+function Index({ routes }: any) {
+  const [, ...rest] = routes;
+
   return (
-    <Navigator defaultIndex={1} routes={['login', '/', 'signup']}>
-      <Tabs>
-        <Login lazy />
-        <SelectMyScreen />
-        <Signup lazy />
-      </Tabs>
-    </Navigator>
-  );
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text style={styles.title}>Index</Text>
 
-function SelectMyScreen({  }: NavigatorScreen) {
-  return (
-    <MyScreen
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-        backgroundColor: 'red',
-      }}
-    >
-      <Text>Select Account Type:</Text>
-      <Link to="login">
-        <Text>Login</Text>
-      </Link>
-      <Link to="signup">
-        <Text>Signup</Text>
-      </Link>
-    </MyScreen>
-  );
-}
-
-function Signup({  }: NavigatorScreen) {
-  return (
-    <MyScreen>
-      <Text>Signup!</Text>
-      <Link to="/app">
-        <Text>Go to app</Text>
-      </Link>
-    </MyScreen>
-  );
-}
-
-function Login({  }: NavigatorScreen) {
-  return (
-    <MyScreen>
-      <Text>Login!</Text>
-      <Link to="/app">
-        <Text>Go to app</Text>
-      </Link>
-    </MyScreen>
-  );
-}
-
-function Feeds({  }: any) {
-  return (
-    <Navigator routes={['home', 'news', 'popular']}>
-      <Headers>
-        <Header>
-          <Text>Home</Text>
-        </Header>
-        <Header>
-          <Text>News</Text>
-        </Header>
-        <Header>
-          <Text>Popular</Text>
-        </Header>
-      </Headers>
-
-      <Tabs>
-        <Feed name="Home" path="home" />
-        <Feed name="News" path="news" />
-        <Feed name="Popular" path="popular" />
-      </Tabs>
-
-      <Tabbar>
-        <Tab to="home" activeStyle={{ borderWidth: 1 }}>
-          <Text>Home</Text>
-        </Tab>
-        <Tab to="news" activeStyle={{ borderWidth: 1 }}>
-          <Text>News</Text>
-        </Tab>
-        <Tab to="popular" activeStyle={{ borderWidth: 1 }}>
-          <Text>Popular</Text>
-        </Tab>
-      </Tabbar>
-    </Navigator>
-  );
-}
-
-const items = Array.from({ length: 15 }).map((v, i) => {
-  return {
-    id: i,
-    text: `Item: ${i}`,
-  };
-});
-
-function Feed({  }: any) {
-  return (
-    <Navigator name="FEED" routes={['/', 'profile/:id']}>
-      {({ state }: any) => (
-        <Stack>
-          <List items={items} path="/" />
-          <Profile path="profile/:id" unmountOnExit item={state.item} />
-        </Stack>
-      )}
-    </Navigator>
-  );
-}
-
-function List({ items }: any) {
-  return (
-    <FlatList
-      style={{ flex: 1, padding: 10 }}
-      contentContainerStyle={{ paddingVertical: 20 }}
-      data={items}
-      renderItem={Card}
-      keyExtractor={(item: any) => `${item.id}`}
-    />
-  );
-}
-
-function Card({ item }: any) {
-  return (
-    <Link to={`profile/${item.id}`} style={styles.card} state={{ item }}>
-      <Text>{`Card: ${item.id}`}</Text>
-    </Link>
-  );
-}
-
-function Profile({ item }: any) {
-  return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'white',
-      }}
-    >
-      {/* <Text>Item: {item.id}</Text> */}
-      <ProfileStack />
-    </View>
-  );
-}
-
-function ProfileStack({}) {
-  return (
-    <Navigator routes={['/', 'two', 'three']}>
-      <Stack>
-        <MyScreen
-          style={{
-            backgroundColor: 'blue',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          path="/"
-        >
-          <Link to="two">
-            <Text>Next</Text>
+      <ScrollView
+        style={{ flex: 1 }}
+        contentContainerStyle={{ alignItems: 'center' }}
+      >
+        {rest.map((route: string) => (
+          <Link
+            to={route}
+            style={{ borderWidth: 1, borderRadius: 4, padding: 10 }}
+          >
+            <Text>{route}</Text>
           </Link>
-        </MyScreen>
-        <MyScreen
-          style={{
-            backgroundColor: 'red',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          path="two"
-        >
-          <Link to="three">
-            <Text>Next</Text>
-          </Link>
-        </MyScreen>
-        <MyScreen
-          style={{
-            backgroundColor: 'green',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-          path="three"
-        >
-          <Text>3</Text>
-        </MyScreen>
-      </Stack>
-    </Navigator>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const containerStyle: StyleProp<ViewStyle> = Platform.select({
-  web: {
-    width: 400,
-    height: 500,
-
-    marginVertical: 10,
-    marginHorizontal: 'auto',
-    overflow: 'hidden',
-  },
-  default: {
-    flex: 1,
-  },
-});
 
 function AppContainer({ children }: any) {
-  return <View style={containerStyle}>{children}</View>;
+  return <View style={styles.appContainer}>{children}</View>;
 }
 
-function MyScreen({ children, style }: any) {
-  return <View style={[styles.container, style]}>{children}</View>;
-}
+const styles = StyleSheet.create({
+  appContainer: Platform.select({
+    web: {
+      width: 400,
+      height: 500,
+      marginVertical: 10,
+      marginHorizontal: 'auto',
+      overflow: 'hidden',
+    },
 
-const styles = {
+    default: {
+      flex: 1,
+    },
+  }),
+
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  homeButton: {
+    width: 200,
+    height: 40,
     borderWidth: 1,
+    borderRadius: 4,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: BOTTOM_SPACE + 10,
   },
 
   card: {
@@ -301,7 +106,13 @@ const styles = {
     justifyContent: 'center',
     alignItems: 'center',
   },
-};
+
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 const APP_NAME = 'example';
 // register the app
